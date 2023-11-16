463c4b088702ad6f02de313ffa0c856ff8ba4481
Illia Leedskiy <leedqvo@gmail.com>
2023-11-16 21:31:15 +0100

    write main window

--- src/trickyfive/Main.java
+++ src/trickyfive/Main.java
@@ -1,10 +1,11 @@
 package trickyfive;
 import javax.swing.JFrame;
 import trickyfive.model.Board;
+import trickyfive.view.MainWindow;
 
 
 public class Main {
     public static void main(String[] args) {
-
+        JFrame mainWindow = new MainWindow();
     }
 }
---
+++ src/trickyfive/view/MainWindow.java
@@ -0,0 +1,61 @@
+package trickyfive.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.border.EmptyBorder;
+
+public class MainWindow extends Window {
+
+    private ArrayList<Window> windows;
+
+    public MainWindow() {
+        windows = new ArrayList<Window>();
+
+        JLabel label = new JLabel();
+        label.setBorder(new EmptyBorder(20, 0, 20, 0));
+        label.setText("Choose the size of the game board");
+
+        JButton button6x6 = createButton("6 x 6", 3);
+        button6x6.setBorder(new EmptyBorder(13, 13, 13, 13));
+
+        JButton button10x10 = createButton("10 x 10", 4);
+        button10x10.setBorder(new EmptyBorder(13, 13, 13, 13));
+
+        JButton button14x14 = createButton("14 x 14", 5);
+        button14x14.setBorder(new EmptyBorder(13, 13, 13, 13));
+
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        getContentPane().add(label);
+        getContentPane().add(button6x6);
+        getContentPane().add(button10x10);
+        getContentPane().add(button14x14);
+
+        this.setVisible(true);
+    }
+
+    private ActionListener getActionListener(int size) {
+        return new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+    }
+
+    private JButton createButton(String text, int size) {
+        JButton button = new JButton(text);
+        button.addActionListener(getActionListener(size));
+        return button;
+    }
+
+    public List<Window> getAllWindows() {
+        return windows;
+    }
+}
